faf9febc1c1ba094c5cd1e1d3cf7b5a04222dac7
Putri Dinar <79742186+putridinar@users.noreply.github.com>
2025-03-10 04:03:43 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,8 @@ simpleCart({
 ],
 	currency: "IDR",
 });
+
+$(document).on('click', '#payWithMidtrans', function () {
+    let totalAmount = $('.simpleCart_grandTotal').text().replace(/[^\d]/g, '');
+    processMidtransPayment(totalAmount);
+});
